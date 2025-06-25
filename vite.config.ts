127fe3b0ecaf4f5af6d0127ad9d8232400118fac
jithin917 <jithin917@gmail.com>
2025-06-25 09:44:02 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
+Import visualizer from rollup-plugin-visualizer ;
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),visualizer()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
