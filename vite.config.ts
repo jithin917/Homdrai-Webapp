06db09cfe8eef5040609d10b0b68d89be4cee1c7
jithin917 <jithin917@gmail.com>
2025-06-27 09:30:52 +0530

    Fix rollup-plugin-visualizer import in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
-import visualizer from 'rollup-plugin-visualizer';
+import { visualizer } from 'rollup-plugin-visualizer';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),visualizer()],
+  plugins: [react(), visualizer()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
